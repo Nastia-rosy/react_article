758423569c4fb38972546b11ddf5ed494b378e1c
Nastia-rosy <nastiiarosy@gmail.com>
2020-04-23 08:02:13 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.c39788355188134f5eb5342f2d6d5099.js"
 );
 
 self.addEventListener('message', (event) => {
